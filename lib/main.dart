1d2d3598eddc36d469121e0d58a3d625a8758b44
K9i-0 <geukfyger@gmail.com>
2023-01-01 09:19:28 +0900

    ✨UI調整系パッケージの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feed_viewer/feature/feed/feed_screen.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: MyApp(),
+    ProviderScope(
+      child: DevicePreview(
+        enabled: const bool.fromEnvironment('enable_device_preview'),
+        builder: (context) => const MyApp(),
+      ),
     ),
   );
 }
@@ -17,7 +21,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter最新記事',
+      // useInheritedMediaQuery、locale、builderは、DevicePreviewに必要
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         // TODO(K9i-0): TabBarのM3対応がstableに入ったらuseMaterial3: trueに変える
         primarySwatch: Colors.blue,
