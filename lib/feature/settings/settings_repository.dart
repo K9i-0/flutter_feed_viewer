04d6060e3c2dcf67f71ff08e53834bf820d74289
K9i-0 <geukfyger@gmail.com>
2023-01-01 11:13:00 +0900

    ✨テーマ設定

---
+++ lib/feature/settings/settings_repository.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feed_viewer/local/shared_preferences.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final settingsRepositoryProvider = Provider.autoDispose<SettingsRepository>(
+  (ref) => SettingsRepository(ref),
+);
+
+class SettingsRepository {
+  final Ref _ref;
+  const SettingsRepository(this._ref);
+
+  ThemeMode get themeMode {
+    final themeModeIndex = _ref
+        .read(sharedPreferencesProvider)
+        .getInt(SharedPreferencesKeys.themeMode.value);
+
+    if (themeModeIndex == null) {
+      return ThemeMode.system;
+    }
+    return ThemeMode.values[themeModeIndex];
+  }
+
+  Future<void> setThemeMode(ThemeMode themeMode) async {
+    await _ref
+        .read(sharedPreferencesProvider)
+        .setInt(SharedPreferencesKeys.themeMode.value, themeMode.index);
+  }
+}
